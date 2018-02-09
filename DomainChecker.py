d7906cf92f3a62841f9ba852d390b3f769e10ec1
CaseyCJohnston <root@cjohnston2.gp.cv.commvault.com>
2018-02-09 15:35:42 -0500

    Parses Files

--- DomainChecker.py
+++ DomainChecker.py
@@ -4,7 +4,13 @@ import sys
 #first check arguments, if arg 1 is a  .txt, then it is a textfile, if  it is not, then it is a single domain
 
 def main():
+	domains = []
 	if sys.argv[1].endswith(".txt"):
 		#parse document and send domains into list
+		domainsFile = open(sys.argv[1], 'r')
+		for line in  domainsFile.readlines():
+			domains.append(line)
 	else:
 		#deal with single domain
+		domains.append(sys.argv[1])
+	print domains
