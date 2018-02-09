c45a2264b7da16f8c54297b80f334e69f5bd7ee7
CaseyCJohnston <root@cjohnston2.gp.cv.commvault.com>
2018-02-09 15:27:57 -0500

    Begins argument checking

--- DomainChecker.py
+++ DomainChecker.py
@@ -1,1 +1,10 @@
+import sys
 #this will be a python script to check a domain or a file of domains
+
+#first check arguments, if arg 1 is a  .txt, then it is a textfile, if  it is not, then it is a single domain
+
+def main():
+	if sys.argv[1].endswith(".txt"):
+		#parse document and send domains into list
+	else:
+		#deal with single domain
