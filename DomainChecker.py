3f851214b092a2ad1c7bac27a509aca08a60c322
CaseyCJohnston <root@cjohnston2.gp.cv.commvault.com>
2018-02-09 15:51:56 -0500

    Parses Arguments

--- DomainChecker.py
+++ DomainChecker.py
@@ -2,15 +2,17 @@ import sys
 #this will be a python script to check a domain or a file of domains
 
 #first check arguments, if arg 1 is a  .txt, then it is a textfile, if  it is not, then it is a single domain
+domains = []
 
-def main():
-	domains = []
-	if sys.argv[1].endswith(".txt"):
-		#parse document and send domains into list
-		domainsFile = open(sys.argv[1], 'r')
-		for line in  domainsFile.readlines():
-			domains.append(line)
-	else:
-		#deal with single domain
-		domains.append(sys.argv[1])
-	print domains
+if sys.argv[1].endswith(".txt"):
+	#parse document and send domains into list
+	
+	unformattedDomains = open(sys.argv[1],'r').read().split('\n')
+	for domain in unformattedDomains:
+		if domain != '':
+			domains.append(domain.strip())
+else:
+	#deal with single domain
+	domains.append(sys.argv[1])
+
+print domains
